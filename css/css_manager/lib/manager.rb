770e137230df366ddd0ed4b656f583103e8145de
James Abbott <james@jamesabbottdd.com>
2011-06-24 15:10:36 +0200

    refactored the CSS Manager

--- css/css_manager/lib/manager.rb
+++ css/css_manager/lib/manager.rb
@@ -1,43 +1,28 @@
-module Quack #both a stack and a queue
-  def quack
-    @quack ||= []
-  end
-
-  def add(char)
-    quack.push(char)
-  end
-
-  def top_two
-    return false if quack.length < 2
-    quack.at(-2) + quack.at(-1)
-  end
-
-  def ready
-    quack.length > 2
-  end
-
-  def get_first
-    quack.shift
-  end
-
-  def length
-    quack.length
-  end
-
-  def clear
-    quack.clear
-    nil
-  end
-
-end
-
 class Manager
-  include Quack
   
   attr_reader :comment
 
   def initialize
+    @queue = []
     @comment = false
+  end
+
+  def get_first
+    @queue.shift
+  end
+
+  def process(char)
+    @queue.push(char)
+    set_state
+    c = filter
+    c unless c.nil?
+  end
+
+  private
+  
+  def top_two
+    return false if @queue.length < 2
+    @queue.at(-2) + @queue.at(-1)
   end
 
   def comment_start
@@ -48,17 +33,23 @@ class Manager
     top_two == '*/'
   end
 
-  def process(char)
-    add(char)
+  def ready
+    @queue.length > 2
+  end
+  
+  def clear
+    @queue.clear
+    nil
+  end
+
+  def set_state
     @comment = true if comment_start
     @comment = false if comment_end
-    c = filter
-    c unless c.nil?
   end
 
   def filter
-    return get_first if ready and comment_start #["c", "/", "*"] - pop "c"
-    clear if comment_end #["*", "/"]
-    get_first if ready and !comment #["a", "b", "c"] or ["b", "c", "/"]
+    return get_first if ready and comment_start #queue = ["c", "/", "*"] - pop "c"
+    clear if comment_end #queue = ["/", "*",  "c", "o", "m", "m", "e", "n", "t", "*", "/"]
+    get_first if ready and !comment #queue = ["a", "b", "c"] or ["b", "c", "/"]
   end
 end
--- css/css_manager/test/test_css_manager.rb
+++ css/css_manager/test/test_css_manager.rb
@@ -1,268 +1,79 @@
-require './lib/manager.rb'
+require '../lib/manager.rb'
 require 'test/unit'
-require './test/test_helper.rb'
+require '../test/test_helper.rb'
 
 class ManagerTest < Test::Unit::TestCase
 
   def setup
-    @m = TestableManager.new
-    @str = 'abcd/*efg*/hij'
-    @foo = '/**/abc'
+    @m = Manager.new
   end
 
-  must "stringify top two elements of the stack when it contains at least 2 elements" do
-    assert_equal @m.top_two, false
-    @m.add 'a'
-    assert_equal @m.top_two, false
-    @m.add 'b'
-    assert_equal @m.top_two, 'ab'
-    @m.add 'c'
-    assert_equal @m.top_two, 'bc'
+  def remove_comments_from_css_string
+    result = ""
+    testfile.each_char do |c|
+      c = @m.process c
+      result << c unless c.nil?
+    end
+    2.times {result << @m.get_first}
+    result
   end
 
-  must "set state correctly on common case (in-string comment)" do
-    e = @str.each_char
-    @m.set_state(e.next)
-    assert_equal @m.comment, false #a
-    @m.set_state(e.next)
-    assert_equal @m.comment, false #b
-    @m.set_state(e.next)
-    assert_equal @m.comment, false #c
-    @m.set_state(e.next)
-    assert_equal @m.comment, false #d
-    @m.set_state(e.next)
-    assert_equal @m.comment, false #/
-    @m.set_state(e.next)
-    assert_equal @m.comment, true #*
-    @m.set_state(e.next)
-    assert_equal @m.comment, true #e
-    @m.set_state(e.next)
-    assert_equal @m.comment, true #f
-    @m.set_state(e.next)
-    assert_equal @m.comment, true #g
-    @m.set_state(e.next)
-    assert_equal @m.comment, true #*
-    @m.set_state(e.next)
-    assert_equal @m.comment, false #/
-    @m.set_state(e.next)
-    assert_equal @m.comment, false #h
+  def test_remove_comments_from_css_string
+    assert_equal remove_comments_from_css_string, uncommented_testfile
   end
+  
+  def testfile
+    "
+    /*comment*/
+    /*another comment*/
 
-  must "set state correctly on edge case (leading empty comment)" do
-    e = @foo.each_char
+    /*concatenated*//*comment*/
 
-    @m.set_state(e.next)
-    #p @m.quack
-    assert_equal @m.length, 1
-    assert_equal @m.top_two, false
-    assert_equal @m.comment_start, false
-    assert_equal @m.comment, false
+    @import 'somefile.css';
 
-    @m.set_state(e.next)
-    #p @m.quack
-    assert_equal @m.length, 2
-    assert_equal @m.top_two, '/*'
-    assert_equal @m.comment_start, true
-    assert_equal @m.comment, true
+    * {margin: 0; padding: 0;}
 
-    @m.set_state(e.next)
-    #p @m.quack
-    assert_equal @m.length, 3
-    assert_equal @m.top_two, '**'
-    assert_equal @m.comment_start, false
-    assert_equal @m.comment, true
+    body {
+        /*
+        multi-line
+        comment
+        */
+        background: #fff;
+        font-size: 62%; /*inline comment*/
+    }
 
-    @m.set_state(e.next)
-    #p @m.quack
-    assert_equal @m.length, 4
-    assert_equal @m.top_two, '*/'
-    assert_equal @m.comment_end, true
-    assert_equal @m.comment, false
+    div#container {/*inline comment*/border: 0; /*trailing comment*/}
 
-    @m.set_state(e.next)
-    #p @m.quack
-    assert_equal @m.length, 5
-    assert_equal @m.top_two, '/a'
-    assert_equal @m.comment_end, false
-    assert_equal @m.comment, false
+    #foo{}/*closing comment*/
+
+    /*also a comment*/#bar{}
+
+    /*final comment*/
+
+    div#container {/*inline comment*/border: 0;}
+    ".gsub!(/\n/, '').gsub!(" ", '')
   end
+    
+  def uncommented_testfile
+    "
+    @import 'somefile.css';
 
-  must "filter out comments on common case" do
-    e = @str.each_char
-    @m.process(e.next) #a
-    assert_equal @m.quack, ["a"]
-    @m.process(e.next) #b
-    assert_equal @m.quack, ["a", "b"]
-    @m.process(e.next) #c
-    assert_equal @m.quack, ["b", "c"]
-    @m.process(e.next) #d
-    assert_equal @m.quack, ["c", "d"]
-    @m.process(e.next) #/
-    assert_equal @m.quack, ["d", "/"]
-    @m.process(e.next) #*
-    assert_equal @m.quack, ["/", "*"]
-    @m.process(e.next) #e
-    assert_equal @m.quack, ["/", "*", "e"]
-    @m.process(e.next) #f
-    assert_equal @m.quack, ["/", "*", "e", "f"]
-    @m.process(e.next) #g
-    assert_equal @m.quack, ["/", "*", "e", "f", "g"]
-    @m.process(e.next) #*
-    assert_equal @m.quack, ["/", "*", "e", "f", "g", "*"]
-    @m.process(e.next) #/
-    assert_equal @m.quack, []
-    @m.process(e.next) #h
-    assert_equal @m.quack, ["h"]
-  end
+    * {margin: 0; padding: 0;}
 
-  must "filter out comments on edge case" do
-    e = @foo.each_char
-    @m.process(e.next) #/
-    assert_equal @m.quack, ["/"]
-    @m.process(e.next) #*
-    assert_equal @m.quack, ["/", "*"]
-    @m.process(e.next) #*
-    assert_equal @m.quack, ["/", "*", "*"]
-    @m.process(e.next) #/
-    assert_equal @m.quack, []
-    @m.process(e.next) #a
-    assert_equal @m.quack, ["a"]
-  end
+    body {
+        background: #fff;
+        font-size: 62%;
+    }
 
-  must "filter out comments from input string and populate output array with correct characters" do
-    e = 'div#foo {/*a comment*/border: 0;}'.each_char
-    assert_equal @m.receiver, []
-    @m.process(e.next) #d
-    assert_equal @m.quack, ["d"]
-    assert_equal @m.receiver, []
+    div#container {border: 0; }
 
-    @m.process(e.next) #i
-    assert_equal @m.quack, ["d", "i"]
-    assert_equal @m.receiver, []
+    #foo{}
 
-    @m.process(e.next) #v
-    assert_equal @m.quack, ["i", "v"]
-    assert_equal @m.receiver, ["d"]
+    #bar{}
 
-    @m.process(e.next) ##
-    assert_equal @m.quack, ["v", "#"]
-    assert_equal @m.receiver, ["d", "i"]
-
-    @m.process(e.next) #f
-    assert_equal @m.quack, ["#", "f"]
-    assert_equal @m.receiver, ["d", "i", "v"]
-
-    @m.process(e.next) #o
-    assert_equal @m.quack, ["f", "o"]
-    assert_equal @m.receiver, ["d", "i", "v", "#"]
-
-    @m.process(e.next) #o
-    assert_equal @m.quack, ["o", "o"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f"]
-
-    @m.process(e.next) #' '
-    assert_equal @m.quack, ["o", " "]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o"]
-
-    @m.process(e.next) #'{'
-    assert_equal @m.quack, [" ", "{"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o"]
-
-    @m.process(e.next) #/
-    assert_equal @m.quack, ["{", "/"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ']
-
-    @m.process(e.next) #*
-    assert_equal @m.quack, ["/", "*"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #a
-    assert_equal @m.quack, ["/", "*", 'a']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #' '
-    assert_equal @m.quack, ["/", "*", 'a', ' ']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #c
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #o
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c', 'o']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #m
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c', 'o', 'm']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #m
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c', 'o', 'm', 'm']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #e
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c', 'o', 'm', 'm', 'e']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #n
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c', 'o', 'm', 'm', 'e', 'n']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #t
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c', 'o', 'm', 'm', 'e', 'n', 't']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #*
-    assert_equal @m.quack, ["/", "*", 'a', ' ', 'c', 'o', 'm', 'm', 'e', 'n', 't', '*']
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #/
-    assert_equal @m.quack, []
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #b
-    assert_equal @m.quack, ["b"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #o
-    assert_equal @m.quack, ["b", "o"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{"]
-
-    @m.process(e.next) #r
-    assert_equal @m.quack, ["o", "r"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b"]
-
-    @m.process(e.next) #d
-    assert_equal @m.quack, ["r", "d"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o"]
-
-    @m.process(e.next) #e
-    assert_equal @m.quack, ["d", "e"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o", "r"]
-
-    @m.process(e.next) #r
-    assert_equal @m.quack, ["e", "r"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o", "r", "d"]
-
-    @m.process(e.next) #:
-    assert_equal @m.quack, ["r", ":"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o", "r", "d", "e"]
-
-    @m.process(e.next) #' '
-    assert_equal @m.quack, [":", " "]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o", "r", "d", "e", "r"]
-
-    @m.process(e.next) #0
-    assert_equal @m.quack, [" ", "0"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o", "r", "d", "e", "r", ":"]
-
-    @m.process(e.next) #;
-    assert_equal @m.quack, ["0", ";"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o", "r", "d", "e", "r", ":", " "]
-
-    @m.process(e.next) #}
-    assert_equal @m.quack, [";", "}"]
-    assert_equal @m.receiver, ["d", "i", "v", "#", "f", "o", "o", ' ', "{", "b", "o", "r", "d", "e", "r", ":", " ", "0"]
+    div#container {border: 0;}
+    ".gsub!(/\n/, '').gsub!(" ", '')
   end
 
 end
+
